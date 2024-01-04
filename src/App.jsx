bf12345b95bd0245a318210e6387db80b3ad22a6
Nate <nathaniel_ramirez@icloud.com>
2024-01-04 09:51:09 -0800

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Button, Box, styled } from "@mui/material";
+import {
+  Button,
+  Box,
+  styled,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Number = styled(Button)({
   borderRadius: "45px",
@@ -66,7 +75,7 @@ function App() {
       return newValue.length <= 9 ? newValue : prevValue;
     });
     if (value === ".") {
-      setOutput("0.")
+      setOutput("0.");
     }
     const clearBtn = document.getElementById("clearBtn");
     const clearText = document.getElementById("clearText");
@@ -101,53 +110,73 @@ function App() {
     if (outputValue.startsWith("-")) {
       setOutput(outputValue.slice(1));
     } else {
-      setOutput(`-${outputValue}`)
+      setOutput(`-${outputValue}`);
     }
-  };  
+  };
 
   return (
-    <div id="screen">
-      <div className="computer">
-        <p id="output">{outputValue}</p>
-      </div>
-      <Box
-        id="buttons"
-        sx={{
-          display: "grid",
-          gap: "10px",
-          gridTemplateColumns: "repeat(4, 90px)",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Grey onClick={clear} id="clearBtn" className="light-grey">
-          <span id="clearText">AC</span>
-        </Grey>
-        <Grey onClick={np} className="light-grey">
-          +/-
-        </Grey>
-        <Grey onClick={percent} className="light-grey">
-          %
-        </Grey>
-        <Orange onClick={() => addInput("/")}>&divide;</Orange>
-        <Number onClick={() => addInput("7")}>7</Number>
-        <Number onClick={() => addInput("8")}>8</Number>
-        <Number onClick={() => addInput("9")}>9</Number>
-        <Orange onClick={() => addInput("*")}>&times;</Orange>
-        <Number onClick={() => addInput("4")}>4</Number>
-        <Number onClick={() => addInput("5")}>5</Number>
-        <Number onClick={() => addInput("6")}>6</Number>
-        <Orange onClick={() => addInput("-")}>-</Orange>
-        <Number onClick={() => addInput("1")}>1</Number>
-        <Number onClick={() => addInput("2")}>2</Number>
-        <Number onClick={() => addInput("3")}>3</Number>
-        <Orange onClick={() => addInput("+")}>+</Orange>
-        <NumberWide onClick={() => addInput("0")}>0</NumberWide>
-        <br />
-        <Number onClick={() => addInput(".")}>.</Number>
-        <Orange onClick={calculate}>=</Orange>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ backgroundColor: "#111" }}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Calculator in React
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </Box>
-    </div>
+      <div id="screen">
+        <div className="computer">
+          <p id="output">{outputValue}</p>
+        </div>
+        <Box
+          id="buttons"
+          sx={{
+            display: "grid",
+            gap: "10px",
+            gridTemplateColumns: "repeat(4, 90px)",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Grey onClick={clear} id="clearBtn" className="light-grey">
+            <span id="clearText">AC</span>
+          </Grey>
+          <Grey onClick={np} className="light-grey">
+            +/-
+          </Grey>
+          <Grey onClick={percent} className="light-grey">
+            %
+          </Grey>
+          <Orange onClick={() => addInput("/")}>&divide;</Orange>
+          <Number onClick={() => addInput("7")}>7</Number>
+          <Number onClick={() => addInput("8")}>8</Number>
+          <Number onClick={() => addInput("9")}>9</Number>
+          <Orange onClick={() => addInput("*")}>&times;</Orange>
+          <Number onClick={() => addInput("4")}>4</Number>
+          <Number onClick={() => addInput("5")}>5</Number>
+          <Number onClick={() => addInput("6")}>6</Number>
+          <Orange onClick={() => addInput("-")}>-</Orange>
+          <Number onClick={() => addInput("1")}>1</Number>
+          <Number onClick={() => addInput("2")}>2</Number>
+          <Number onClick={() => addInput("3")}>3</Number>
+          <Orange onClick={() => addInput("+")}>+</Orange>
+          <NumberWide onClick={() => addInput("0")}>0</NumberWide>
+          <br />
+          <Number onClick={() => addInput(".")}>.</Number>
+          <Orange onClick={calculate}>=</Orange>
+        </Box>
+      </div>
+    </>
   );
 }
 
